b63c9cfc739f93ef208c377ad84fc832ab83bfb7
Vinicius Crestani <vinicr@gmail.com>
2020-01-31 15:31:44 -0300

    Notificacoes

--- src/store/modules/auth/sagas.js
+++ src/store/modules/auth/sagas.js
@@ -56,7 +56,7 @@ export function setToken({ payload }) {
   const { token } = payload.auth;
 
   if (token) {
-    api.defaults.headers.Authorization = token;
+    api.defaults.headers.Authorization = `Bearer ${token}`;
   }
 }
 
